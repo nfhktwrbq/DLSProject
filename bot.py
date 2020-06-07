26487dac045d94e0dd246d6f05302028081b76d9
nfhktwrb <nfhktwrbq@yandex.ru>
2020-06-07 05:00:47 +0300

    Edit webhook

--- bot.py
+++ bot.py
@@ -184,7 +184,7 @@ if __name__ == '__main__':
     #executor.start_polling(dp)
     executor.start_webhook(
         dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
+        webhook_path="",#WEBHOOK_PATH,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         skip_updates=True,
